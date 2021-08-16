719cd639c7607c728a0f998f2d9ba41cd0132b05
Sergio <7ergio@gmail.com>
2021-08-16 16:18:03 +0200

    Added messi

--- js/Lyuba/messi.js
+++ js/Lyuba/messi.js
@@ -1,16 +1,5 @@
-const promptLiga = prompt("Enter number of goals in LaLiga");
-const promptCodaDelRey = prompt("Enter number of goals in CodaDelRey");
-const promptChampions = prompt("Enter number of goals in Champions");
-const laLigaGoals = Number(promptLiga);
-const codaDelReyGoals = Number(promptCodaDelRey);
-const championsLeagueGoals = Number(promptChampions);
-alert ("Total number of goals in all three leagues is" + toyalGoals)
-
-
-
-
-function Add (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    // code goes here
-    const result = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-    return result
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+      // code goes here
+      return (laLigaGoals + copaDelReyGoals + championsLeagueGoals) 
   }
+  goals(5, 10, 2);
